cec9258c874166c85372ef5526eed64bbfc3d755
Eunji Kang <etothej@naver.com>
2022-08-05 17:42:52 +0900

    added

--- js/main.js
+++ js/main.js
@@ -13,4 +13,19 @@ let subMenu = document.querySelector('.submenu')
 blog.addEventListener('click', () => {
   blog.classList.toggle('blog_active');
   subMenu.classList.toggle('submenu_open')
-})
+});
+
+
+let scrollBtn = document.querySelector('.scroll_btn');
+
+scrollBtn.addEventListener('click', () => {
+  window.scrollTo(0, about);
+});
+
+
+let about = document.querySelector('.about').offsetTop;
+
+
+
+
+console.log(about);
