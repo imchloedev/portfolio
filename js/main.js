ff03aeda9b76681188279e149e69cf3247ee7cfc
Eunji Kang <etothej@naver.com>
2022-08-11 19:48:41 +0900

    updated

--- js/main.js
+++ js/main.js
@@ -1,42 +1,75 @@
-// window.addEventListener("DOMContentLoaded", function () {
-//   setTimeout(() => {
-//     document.querySelector(".loader").style.display = "none";
-//     document.querySelector("body").style.overflow = "auto";
-//   }, 3000);
-// });
-
-
-
 function isLoading() {
-    setTimeout(() => {
-    document.querySelector(".loader").classList.add('remove');
+  setTimeout(() => {
+    document.querySelector(".loader").classList.add("remove");
     document.querySelector("body").style.overflow = "auto";
   }, 3000);
 }
 
 isLoading();
 
-
-
 let navBtn = document.querySelector(".nav_btn button");
-let nav = document.querySelector("nav");
+let mNav = document.querySelector(".mNav");
 
 navBtn.addEventListener("click", () => {
   navBtn.classList.toggle("nav_btn_active");
-  nav.classList.toggle("nav_open");
+  mNav.classList.toggle("open");
 });
 
-let blog = document.querySelector(".blog");
-let subMenu = document.querySelector(".submenu");
-
-blog.addEventListener("click", () => {
-  blog.classList.toggle("blog_active");
-  subMenu.classList.toggle("submenu_open");
+document.querySelectorAll(".mNav ul li a").forEach((el) => {
+  el.addEventListener("click", (e) => {
+    mNav.classList.remove("open");
+    navBtn.classList.remove("nav_btn_active");
+  });
 });
 
 let scrollBtn = document.querySelector(".scroll_btn");
+let aboutTop = document.querySelector(".about").offsetTop;
 
 scrollBtn.addEventListener("click", () => {
-  let aboutTop = document.querySelector(".about").offsetTop;
   window.scrollTo(0, aboutTop);
 });
+
+// console.log(aboutTop);
+
+let prevScrollTop = window.pageYOffset;
+
+window.addEventListener("scroll", () => {
+  let currScrollTop =
+    window.pageYOffset || document.documentElement.scrollTop || window.scrollY;
+
+  if (prevScrollTop < currScrollTop) {
+    document.querySelector("header").classList.add("hide");
+  } else {
+    document.querySelector("header").classList.remove("hide");
+  }
+
+  prevScrollTop = currScrollTop;
+
+  if (currScrollTop > aboutTop) {
+    document.querySelector("header").classList.add("active");
+  } else {
+    document.querySelector("header").classList.remove("active");
+  }
+
+  scrollProgress();
+});
+
+function scrollProgress() {
+  let scrollTop =
+    (document.documentElement.scrollTop ||
+      window.scrollY ||
+      window.pageYOffset) +
+    window.innerHeight / 2;
+
+  // console.log(scrollTop);
+  // console.log(window.innerHeight);
+
+  // content_item
+  document.querySelectorAll(".content_item").forEach((el) => {
+    if (scrollTop > el.offsetTop) {
+      el.classList.add("show");
+    } else {
+      el.classList.remove("show");
+    }
+  });
+}
